8b70817588a6faaad6597a0f28520c768e7d6284
phantombass <tmkeaton@ngu.edu>
2021-05-10 06:49:53 -0400

    Fix to Escape Rope

--- Plugins/Pokemon Tempest Full Script/01_Phantombass_Script_Changes.rb
+++ Plugins/Pokemon Tempest Full Script/01_Phantombass_Script_Changes.rb
@@ -2372,6 +2372,30 @@ ItemHandlers::UseFromBag.add(:ESCAPEROPE,proc { |item|
   pbMessage(_INTL("Can't use that here."))
   next 0
 })
+ItemHandlers::UseInField.add(:ESCAPEROPE,proc { |item|
+  escape = ($PokemonGlobal.escapePoint rescue nil)
+  if !escape || escape==[]
+    pbMessage(_INTL("Can't use that here."))
+    next 0
+  end
+  if $game_player.pbHasDependentEvents?
+    pbMessage(_INTL("It can't be used when you have someone with you."))
+    next 0
+  end
+  pbUseItemMessage(item)
+  pbFadeOutIn {
+    $game_temp.player_new_map_id    = escape[0]
+    $game_temp.player_new_x         = escape[1]
+    $game_temp.player_new_y         = escape[2]
+    $game_temp.player_new_direction = escape[3]
+    pbCancelVehicles
+    $scene.transfer_player
+    $game_map.autoplay
+    $game_map.refresh
+  }
+  pbEraseEscapePoint
+  next 2
+})
 ItemHandlers::UseOnPokemon.add(:RARECANDY,proc { |item,pkmn,scene|
   if pkmn.level>=GameData::GrowthRate.max_level || pkmn.shadowPokemon? || pkmn.level>=$game_variables[106]
     scene.pbDisplay(_INTL("It won't have any effect."))
